20163d79d5cc0e6c4ca5184b2a45edaa41729256
Andrzej Żuraw <azuraw10@gmail.com>
2020-05-24 07:49:30 +0200

    Napraw położenie srób

--- dron.cc
+++ dron.cc
@@ -28,14 +28,17 @@ Dron::Dron(drawNS::APIGnuPlot3D *gnuplotAPI, int x, int y, int z, float a, float
     }
 
     // Wyznaczam bok dla śrób - sróby powinny nie być za duże w stosunku do drona
-    double bokSroby = std::min(a, b) / 3;
+    double bokSroby = std::min(c, b) / 3;
+    double HSroby = 1;
+    Wektor w;
+    w[0] = HSroby / 2;
     // Dodaj sroby z "tyłu" drona
-    auto s1 = std::unique_ptr<Sroba>(new Sroba(gnuplotAPI, wierzcholki[6],bokSroby,0.5));
+    auto s1 = std::unique_ptr<Sroba>(new Sroba(gnuplotAPI, wierzcholki[2] + w,bokSroby,HSroby));
     s1->ustawKolor("green");
     // musimy przenieść, żeby uniknąć błędów kompilacji
     obiektyZalezne.push_back(std::move(s1));
 
-    auto s2 = std::unique_ptr<Sroba>(new Sroba(gnuplotAPI, wierzcholki[5],bokSroby,0.5));
+    auto s2 = std::unique_ptr<Sroba>(new Sroba(gnuplotAPI, wierzcholki[1] + w,bokSroby,HSroby));
     s2->ustawKolor("green");
     obiektyZalezne.push_back(std::move(s2));
 }
--- sroba.cpp
+++ sroba.cpp
@@ -11,8 +11,8 @@ Sroba::Sroba(drawNS::APIGnuPlot3D *gnuplotAPI, const drawNS::Point3D &p, double 
     S[1]=p[1];
     S[2]=p[2];
 
-    double zDolnej = p[2] - H / 2;
-    double zGornej = p[2] + H / 2;
+    double xDolnej = p[0] - H / 2;
+    double xGornej = p[0] + H / 2;
 
     /* Musimy wyznaczyć położenia tylko 2 wierzchołków.
     // Reszte wyznaczamy na podstawie tych 2
@@ -23,24 +23,24 @@ Sroba::Sroba(drawNS::APIGnuPlot3D *gnuplotAPI, const drawNS::Point3D &p, double 
     //    \___/
     */
     double h = a * sqrt(3) / 2;
-    double x0 = S[0] - a;
+    double z0 = S[2] - a;
     double y0 = S[1];
-    double x1 = S[0] - 0.5 * a;
+    double z1 = S[2] - 0.5 * a;
     double y1 = S[1] + h;
 
-    wierzcholki[0][0] = x0;
+    wierzcholki[0][0] = xDolnej;
     wierzcholki[0][1] = y0;
-    wierzcholki[0][2] = zDolnej;
+    wierzcholki[0][2] = z0;
 
-    wierzcholki[1][0] = x1;
+    wierzcholki[1][0] = xDolnej;
     wierzcholki[1][1] = y1;
-    wierzcholki[1][2] = zDolnej;
+    wierzcholki[1][2] = z1;
 
     wierzcholki[2] = wierzcholki[1];
-    wierzcholki[2][0] += a;
+    wierzcholki[2][2] += a;
 
     wierzcholki[3] = wierzcholki[0];
-    wierzcholki[3][0] += 2 * a;
+    wierzcholki[3][2] += 2 * a;
 
     wierzcholki[4] = wierzcholki[2];
     wierzcholki[4][1] -= 2 * h;
@@ -49,12 +49,12 @@ Sroba::Sroba(drawNS::APIGnuPlot3D *gnuplotAPI, const drawNS::Point3D &p, double 
     wierzcholki[5][1] -= 2 * h;
 
     for (uint i = 0; i < 6; ++i) {
-      wierzcholki[i][2] = zDolnej;
+      wierzcholki[i][0] = xDolnej;
     }
 
     for (uint i = 6; i < 12; ++i) {
       wierzcholki[i] = wierzcholki[i - 6];
-      wierzcholki[i][2] = zGornej;
+      wierzcholki[i][0] = xGornej;
     }
 }
 
@@ -75,7 +75,7 @@ std::vector<std::pair<uint, uint> > Sroba::powierzchnie() const
 double Sroba::kierunekXY() const
 {
     const Wektor w1 = wierzcholki[0];
-    const Wektor w2 = wierzcholki[3];
+    const Wektor w2 = wierzcholki[6];
     double delta_x = w1[0] - w2[0];
     double delta_y = w1[1] - w2[1];
     double theta_radians = atan2(delta_y, delta_x);
